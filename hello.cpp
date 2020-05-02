ed561e7fcf46fc89404c8287cea1de4b839c2a70
Juan Chavez <juanechavez16@gmail.com>
2020-05-02 00:38:39 -0400

    added addition

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+int add(const int &x, const int &y){
+    return x + y;
+}
+
 int main(){
     std::cout << "Hello World!" << std::endl;
+    int x = 7;
+    int y = 3;
+    std::cout << add(x, y) << std::endl;
 }
