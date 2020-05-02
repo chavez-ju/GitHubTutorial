908d8c7a1731461c32a9776ef4ffe9b7a47a13e7
Juan Chavez <juanechavez16@gmail.com>
2020-05-02 01:17:48 -0400

    replace with mult

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-int division(const int &x, const int &y){
-    return x / y;
+int mult(const int &x, const int &y){
+    return x * y;
 }
 
 int main(){
     std::cout << "Hello World!" << std::endl;
     int x = 7;
     int y = 3;
-    std::cout << division(x, y) << std::endl;
+    std::cout << mult(x, y) << std::endl;
 }
