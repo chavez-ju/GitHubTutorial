0f137dec2213c7db4cdf01a623ed83560b3b44fa
Juan Chavez <juanechavez16@gmail.com>
2020-05-02 00:52:06 -0400

    div replacement

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-int add(const int &x, const int &y){
-    return x + y;
+int division(const int &x, const int &y){
+    return x / y;
 }
 
 int main(){
     std::cout << "Hello World!" << std::endl;
     int x = 7;
     int y = 3;
-    std::cout << add(x, y) << std::endl;
+    std::cout << division(x, y) << std::endl;
 }
